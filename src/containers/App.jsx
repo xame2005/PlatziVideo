00b4ffebccb0a82bd618d498f2c1213bf7bcc825
Samuel Gonzalez <xame2005@gmail.com>
2020-08-09 20:37:25 -0500

    useState y useEffect

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../components/Header";
 import Search from "../components/Search";
 import Categories from "../components/Categories";
@@ -7,38 +7,52 @@ import CarouselItem from "../components/CarouselItem";
 import "../assets/styles/App.scss";
 import Footer from "../components/Footer";
 
-const App=()=>(
+const API = "http://localhost:3000/initialState"
+
+const App = () => {
+    const [videos, setVideos] = useState([]);
+    
+    useEffect(()=>{
+        fetch(API)
+        .then(response=> response.json())
+        .then(data=> setVideos(data))
+    }, []);
+
+    console.log(videos);
+
+  return (
     <div className="App">
-        <Header/>
-        <Search/>
-        <Categories title="Lo más popular">
-            <Carousel>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
-            </Carousel>
-        </Categories>
+      <Header />
+      <Search />
+      <Categories title="Lo más popular">
+        <Carousel>
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+        </Carousel>
+      </Categories>
 
-        <Categories title="Tendencias">
-            <Carousel>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
-            </Carousel>
-        </Categories>
+      <Categories title="Tendencias">
+        <Carousel>
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+        </Carousel>
+      </Categories>
 
-        <Categories title="Novedades">
-            <Carousel>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
-            </Carousel>
-        </Categories>
-        <Footer/>
+      <Categories title="Novedades">
+        <Carousel>
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+        </Carousel>
+      </Categories>
+      <Footer />
     </div>
-);
+  );
+};
 
 export default App;
